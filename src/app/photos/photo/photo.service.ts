6b82583d8bbd2b955e48fd5c6c891ca615a2717b
gugabala <gugabala@gmail.com>
2020-11-22 10:42:28 -0300

    Segmentos de rotas PRÓXIMA ATIVIDADE

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,3 +1,4 @@
+import { Photo } from './photo';
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 
@@ -10,7 +11,7 @@ export class PhotoService {
     constructor(private http: HttpClient) {}
 
     listFromUser(userName: string) {
-        return this.http
-            .get<any[]>(API + '/' + userName + '/photos');
-    }
+      return this.http
+          .get<Photo[]>(API + '/' + userName + '/photos');
+  }
 }
